6b737c3622a665f13ff1eff6185042ae64404822
Richard Xu <richard.xu03@sap.com>
2021-11-01 14:45:17 +0800

    use h2

--- src/main/java/com/richard/demo/DemoApplication.java
+++ src/main/java/com/richard/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.richard.demo;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 名字无所谓，但是要有SpringBootApplication注解
@@ -14,10 +18,23 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
  */
 @SpringBootApplication
 @ServletComponentScan
-public class DemoApplication {
+@Slf4j
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+    private final JdbcTemplate template;
+
+    public DemoApplication(JdbcTemplate template) {
+        this.template = template;
+    }
+
+    @Override
+    public void run(String... args) {
+        String sql = "SELECT count(*)  FROM USR";
+        int count = template.queryForObject(sql, Integer.class);
+        log.info("user count is " + count);
+    }
 }
