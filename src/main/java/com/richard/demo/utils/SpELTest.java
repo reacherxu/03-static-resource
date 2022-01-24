25de91acc588821a7a998d108c2ee29b08ca2b58
I350644 <richard.xu03@sap.com>
2022-01-24 15:54:16 +0800

    SpEL and ReflectionUtil

--- src/main/java/com/richard/demo/utils/SpELTest.java
+++ src/main/java/com/richard/demo/utils/SpELTest.java
@@ -1,11 +1,16 @@
 package com.richard.demo.utils;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.junit.Test;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.expression.BeanFactoryResolver;
 import org.springframework.context.expression.MapAccessor;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.EvaluationException;
@@ -16,6 +21,7 @@ import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 import com.google.gson.Gson;
+import com.richard.demo.services.impl.OrderInfoDaoAImpl;
 import com.richard.demo.utils.pojos.SubClass1;
 import com.richard.demo.utils.pojos.TestClass1;
 
@@ -31,17 +37,102 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SpELTest {
 
-    // 输出一个简单的字符串“Hello，编程帮”。
+    /**
+     * SpEL支持的字面量包括：字符串、数字类型（int、long、float、double）、布尔类型、null类型。
+     * 输出一个简单的字符串“Hello，编程帮”。
+     */
     @Test
-    public void testStr() {
+    public void testBasic() {
         // 构造解析器
         ExpressionParser parser = new SpelExpressionParser();
         // 解析器解析字符串表达式
         Expression exp = parser.parseExpression("'Hello，编程帮'");
         // 获取表达式的值
         String message = (String) exp.getValue();
-        System.out.println(message);
+        System.out.println("message is " + message);
+        String str1 = parser.parseExpression("'Hello World!'").getValue(String.class);
+        int int1 = parser.parseExpression("1").getValue(Integer.class);
+        long long1 = parser.parseExpression("-1L").getValue(long.class);
+        float float1 = parser.parseExpression("1.1").getValue(Float.class);
+        double double1 = parser.parseExpression("1.1E+2").getValue(double.class);
+        int hex1 = parser.parseExpression("0xa").getValue(Integer.class);
+        long hex2 = parser.parseExpression("0xaL").getValue(long.class);
+        boolean true1 = parser.parseExpression("true").getValue(boolean.class);
+        boolean false1 = parser.parseExpression("false").getValue(boolean.class);
+        Object null1 = parser.parseExpression("null").getValue(Object.class);
+
+        System.out.println("str1=" + str1);
+        System.out.println("int1=" + int1);
+        System.out.println("long1=" + long1);
+        System.out.println("float1=" + float1);
+        System.out.println("double1=" + double1);
+        System.out.println("hex1=" + hex1);
+        System.out.println("hex2=" + hex2);
+        System.out.println("true1=" + true1);
+        System.out.println("false1=" + false1);
+        System.out.println("null1=" + null1);
     }
+
+    /**
+     * 类类型表达式
+     * 使用“T(Type)”来表示java.lang.Class实例，“Type”必须是类全限定名，“java.lang”包除外，即该包下的类可以不指定包名；
+     * 使用类类型表达式还可以进行访问类静态方法及类静态字段。
+     */
+    @Test
+    public void testClassTypeExpression() {
+        ExpressionParser parser = new SpelExpressionParser();
+        // java.lang包类访问
+        Class<String> result1 = parser.parseExpression("T(String)").getValue(Class.class);
+        System.out.println(result1);
+
+        // 其他包类访问
+        String expression2 = "T(com.richard.demo.utils.JacksonUtil)";
+        Class<JacksonUtil> value = parser.parseExpression(expression2).getValue(Class.class);
+        System.out.println(value == JacksonUtil.class);
+
+        // 类静态字段访问
+        int result3 = parser.parseExpression("T(Integer).MAX_VALUE").getValue(int.class);
+        System.out.println(result3 == Integer.MAX_VALUE);
+
+        // 类静态方法调用
+        int result4 = parser.parseExpression("T(Integer).parseInt('1')").getValue(int.class);
+        System.out.println(result4);
+        // 可以进行访问类静态方法
+        EvaluationContext context = new StandardEvaluationContext();
+        TestClass1 request = TestClass1.builder().name("tc").married(false).age(10)
+                .sub(SubClass1.builder().subPropertyA("a").subPropertyB(2).build()).build();
+        context.setVariable("obj", request);
+        String expression2_1 = "T(com.richard.demo.utils.JacksonUtil).writeStr(#obj)";
+        String result = parser.parseExpression(expression2_1).getValue(context, String.class);
+        System.out.println(result);
+    }
+
+    /**
+     * 类实例化
+     * 类实例化同样使用java关键字“new”，类名必须是全限定名，但java.lang包内的类型除外，如String、Integer。
+     * SpEL支持instanceof运算符，跟Java内使用同义
+     */
+    @Test
+    public void testConstructorExpression() {
+        ExpressionParser parser = new SpelExpressionParser();
+        String result1 = parser.parseExpression("new String('路人甲java')").getValue(String.class);
+        System.out.println(result1);
+
+        Date result2 = parser.parseExpression("new java.util.Date()").getValue(Date.class);
+        System.out.println(result2);
+
+        // 实例化对象，无需使用反射
+        OrderInfoDaoAImpl order =
+                parser.parseExpression("new com.richard.demo.services.impl.OrderInfoDaoAImpl()").getValue(OrderInfoDaoAImpl.class);
+        String result = order.queryOrderList();
+        log.info("order result is {}", result);
+
+        ExpressionParser newParser = new SpelExpressionParser();
+        Boolean value = newParser.parseExpression("'路人甲' instanceof T(String)").getValue(Boolean.class);
+        System.out.println(value);
+
+    }
+
 
     // SpEL 还可以调用方法、访问属性和调用构造函数。
     @Test
@@ -64,7 +155,16 @@ public class SpELTest {
         }
     }
 
-    // 测试变量
+    /**
+     * 测试变量
+     * 1）创建解析器：SpEL使用ExpressionParser接口表示解析器，提供SpelExpressionParser默认实现；
+     *
+     * 2）解析表达式：使用ExpressionParser的parseExpression来解析相应的表达式为Expression对象。
+     *
+     * 3）构造上下文：准备比如变量定义等等表达式需要的上下文数据。
+     *
+     * 4）求值：通过Expression接口的getValue方法根据上下文获得表达式值。
+     */
     @Test
     public void test1() {
         ExpressionParser parser = new SpelExpressionParser();
@@ -74,6 +174,11 @@ public class SpELTest {
         System.out.println(expression.getValue(context));
     }
 
+    /**
+     * 变量定义通过EvaluationContext接口的setVariable(variableName, value)方法定义；
+     * 在表达式中使用"#variableName"引用；
+     * 除了引用自定义变量，SpE还允许引用根对象及当前上下文对象，使用"#root"引用根对象，使用"#this"引用当前上下文对象；
+     */
     @Test
     public void testVariableExpression() {
         ExpressionParser parser = new SpelExpressionParser();
@@ -209,6 +314,37 @@ public class SpELTest {
 
     }
 
+    /**
+     * 列表，字典，数组元素修改
+     * 可以使用赋值表达式或Expression接口的setValue方法修改；
+     */
+    @Test
+    public void testCollection() {
+        ExpressionParser parser = new SpelExpressionParser();
+
+        // 修改list元素值
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+
+        EvaluationContext context1 = new StandardEvaluationContext();
+        context1.setVariable("collection", list);
+        parser.parseExpression("#collection[1]").setValue(context1, 4);
+        int result1 = parser.parseExpression("#collection[1]").getValue(context1, int.class);
+        System.out.println(result1);
+        System.out.println(list);
+
+        // 修改map元素值
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("a", 1);
+        EvaluationContext context2 = new StandardEvaluationContext();
+        context2.setVariable("map", map);
+        parser.parseExpression("#map['a']").setValue(context2, 4);
+        Integer result2 = parser.parseExpression("#map['a']").getValue(context2, int.class);
+        System.out.println(result2);
+        System.out.println(map);
+    }
+
     @Data
     public static class Car {
         private String name;
@@ -233,6 +369,9 @@ public class SpELTest {
         }
     }
 
+    /**
+     * 对象属性存取及安全导航表达式
+     */
     @Test
     public void test5() {
         User user = new User();
@@ -254,6 +393,28 @@ public class SpELTest {
         user.setCar(car);
 
         System.out.println(parser.parseExpression("#user?.car?.toString()").getValue(context, String.class));
+    }
+
+    /**
+     * SpEL支持使用“@”符号来引用Bean，在引用Bean时需要使用BeanResolver接口实现来查找Bean，
+     * Spring提供BeanFactoryResolver实现。
+     */
+    @Test
+    public void test6() {
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        User user = new User();
+        Car car = new Car();
+        car.setName("保时捷");
+        user.setCar(car);
+        factory.registerSingleton("user", user);
+
+        StandardEvaluationContext context = new StandardEvaluationContext();
+        context.setBeanResolver(new BeanFactoryResolver(factory));
+
+        ExpressionParser parser = new SpelExpressionParser();
+        User userBean = parser.parseExpression("@user").getValue(context, User.class);
+        System.out.println(userBean);
+        System.out.println(userBean == factory.getBean("user"));
     }
 
 }
--- src/main/java/com/richard/demo/utils/reflection/ReflectionUtil.java
+++ src/main/java/com/richard/demo/utils/reflection/ReflectionUtil.java
@@ -2,6 +2,7 @@ package com.richard.demo.utils.reflection;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Objects;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,11 +10,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ReflectionUtils;
 
 import com.richard.demo.DemoApplication;
+import com.richard.demo.services.impl.OrderInfoDaoAImpl;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 这是一个起spring boot 的test
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {DemoApplication.class})
 @Slf4j
@@ -64,6 +71,56 @@ public class ReflectionUtil {
         // 执行方法
         Object result = method.invoke(obj, "first", "second");
         log.info(result.toString());
+
+
+        /// for next test case
+        clzss = OrderInfoDaoAImpl.class;
+        obj = clzss.getDeclaredConstructor().newInstance();
+        // 获取执行方法对象.形参是待执行方法名
+        method = clzss.getDeclaredMethod("queryOrderList");
+        method.setAccessible(true);
+        // 执行方法
+        result = method.invoke(obj);
+        log.info(result.toString());
+    }
+
+    @Test
+    public void test() {
+
+        try {
+            // 调用非静态方法
+            Class<?> order = ClassUtils.forName("com.richard.demo.services.impl.OrderInfoDaoAImpl", ClassUtils.getDefaultClassLoader());
+            Method method = ReflectionUtils.findMethod(order, "queryOrderList");
+            if (Objects.nonNull(method)) {
+                Object obj = order.getDeclaredConstructor().newInstance();
+                Object result = ReflectionUtils.invokeMethod(method, obj);
+                log.info("order result is {}", result.toString());
+            }
+
+            // 调用静态方法
+            Class<?> julBridge = ClassUtils.forName("org.slf4j.bridge.SLF4JBridgeHandler", ClassUtils.getDefaultClassLoader());
+            Method removeHandlers = ReflectionUtils.findMethod(julBridge, "removeHandlersForRootLogger");
+            if (removeHandlers != null) {
+                log.info("Removing all previous handlers for JUL to SLF4J bridge");
+                ReflectionUtils.invokeMethod(removeHandlers, null);
+            }
+
+            Method install = ReflectionUtils.findMethod(julBridge, "install");
+            if (install != null) {
+                log.info("Installing JUL to SLF4J bridge");
+                ReflectionUtils.invokeMethod(install, null);
+            }
+        } catch (ClassNotFoundException | NoSuchMethodException ignored) {
+            // Indicates the java.util.logging bridge is not in the classpath. This is not an indication of a
+            // problem.
+            log.info("JUL to SLF4J bridge is not available on the classpath");
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 
 }
