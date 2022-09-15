0976be500719d82e096010d3e9b284d2d6d299c8
Xu, Richard <richard.xu03@sap.com>
2022-09-15 10:11:47 +0800

    compress stream

--- src/main/java/com/richard/demo/utils/FileUtil.java
+++ src/main/java/com/richard/demo/utils/FileUtil.java
@@ -1,16 +1,12 @@
 package com.richard.demo.utils;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.*;
+import java.nio.charset.Charset;
 import java.util.Date;
 import java.util.List;
+import java.util.zip.Deflater;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 import org.junit.Test;
@@ -58,6 +54,7 @@ public class FileUtil {
             log.error("Error occurred while zip file {} ", srcFile.getName());
         }
     }
+
 
     /**
      * @param content file content
@@ -149,16 +146,136 @@ public class FileUtil {
         return new FileSystemResource(outFile.getPath());
     }
 
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws IOException {
         // Resource file = test1();
         // log.info(file.getFilename());
-        String dir = "temp";
-        File file = new File(getTempLoaction() + "/" + dir);
-        createDirectory(file);
-        createDirectory(new File(getTempLoaction() + "/" + dir + "/bbb"));
+        // String dir = "temp";
+        // File file = new File(getTempLoaction() + "/" + dir);
+        // createDirectory(file);
+        // createDirectory(new File(getTempLoaction() + "/" + dir + "/bbb"));
+        //
+        // zipFiles(Arrays.asList(file), new File(getTempLoaction() + "/" + dir + ".zip"));
 
-        zipFiles(Arrays.asList(file), new File(getTempLoaction() + "/" + dir + ".zip"));
+        // 不存文件的压缩方式
+        String before = readFromFile(getTempLoaction() + "test2.txt");
+
+        byte[] bytes = zipByteArrayOutputStream(before.getBytes(), "test.zip");
+        System.out.println(new String(bytes, Charset.defaultCharset()));
+        FileUtil.writeFile(new String(bytes, Charset.defaultCharset()), FileUtil.getTempLoaction() + "test10.txt");
+
+        ByteArrayInputStream bin = new ByteArrayInputStream(bytes);
+
+        byte[] byte1 = uncompress(bin);
+        FileUtil.writeFile(new String(byte1, Charset.defaultCharset()), FileUtil.getTempLoaction() + "test11.txt");
+
+        System.out.println(new String(byte1, Charset.defaultCharset()));
+
+        readFromFile(FileUtil.getTempLoaction() + "test2.txt");
     }
+
+    /**
+     * 从文件中读取文本
+     */
+    public static String readFromFile(String filename) throws IOException {
+
+        BufferedInputStream bufferedInput = null;
+        byte[] buffer = new byte[1024];
+        StringBuilder sb = new StringBuilder();
+
+        try {
+
+            // 创建BufferedInputStream 对象
+            bufferedInput = new BufferedInputStream(new FileInputStream(filename));
+
+            int bytesRead = 0;
+
+            // 从文件中按字节读取内容，到文件尾部时read方法将返回-1
+            while ((bytesRead = bufferedInput.read(buffer)) != -1) {
+
+                // 将读取的字节转为字符串对象
+                String chunk = new String(buffer, 0, bytesRead);
+                sb.append(chunk);
+            }
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            // 关闭 BufferedInputStream
+            try {
+                if (bufferedInput != null)
+                    bufferedInput.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 文件流压缩
+     * 
+     * @param baisByte 需要压缩的字节输出流(ByteArrayOutputStream)的字节数组
+     * @param fileName 需要压缩的文件名
+     * @return 压缩后字节数组输出流转为的字符串
+     * @throws IOException
+     */
+    public static byte[] zipByteArrayOutputStream(byte[] baisByte, String fileName) {
+        byte[] result = null;
+
+        try (// 1.将需要压缩的字节输出流，转为字节数组输入流，
+                ByteArrayInputStream bais = new ByteArrayInputStream(baisByte);
+                // 2.创建字节数组输出流，用于返回压缩后的输出流字节数组
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                // 3.创建压缩输出流
+                ZipOutputStream zipOut = new ZipOutputStream(baos);) {
+
+
+            zipOut.setMethod(ZipOutputStream.DEFLATED);// 进行压缩存储
+            zipOut.setLevel(Deflater.BEST_COMPRESSION);// 压缩级别值为0-9共10个级别(值越大，表示压缩越利害)
+            // 4.设置ZipEntry对象，并对需要压缩的文件命名
+            zipOut.putNextEntry(new ZipEntry(fileName));
+
+            // 5.读取要压缩的字节输出流，进行压缩
+            int temp = 0;
+            while ((temp = bais.read()) != -1) {
+                zipOut.write(temp); // 压缩输出
+            }
+            zipOut.closeEntry();
+            result = baos.toByteArray();
+        } catch (IOException e) {
+            log.error("[zipByteArrayOutputStream] zip file failed, file name is {}", fileName);
+        }
+
+        return result;
+    }
+
+    // refer to https://blog.csdn.net/weixin_42919496/article/details/81518931
+    // https://blog.csdn.net/u012909738/article/details/81536761
+    public static byte[] uncompress(InputStream inputStream) {
+        byte[] content = new byte[10240];
+        try (ZipInputStream zis = new ZipInputStream(inputStream)) {
+            ZipEntry ze = null;
+            while (((ze = zis.getNextEntry()) != null) && !ze.isDirectory()) {
+                String name = ze.getName();
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                byte[] buffer = new byte[10240];
+                int length = -1;
+                while ((length = zis.read(buffer, 0, buffer.length)) > -1) {
+                    byteArrayOutputStream.write(buffer, 0, length);
+                }
+                content = byteArrayOutputStream.toByteArray();
+                byteArrayOutputStream.close();
+            }
+        } catch (IOException e) {
+            log.error("[uncompress] uncompress file failed", e);
+        }
+        return content;
+    }
+
 
 
     private static String prepareCdt() {
