e28c287f0fb6cd63d56f623b68acdb4037daa175
Xu, Richard <richard.xu03@sap.com>
2024-11-01 17:06:28 +0800

    fix error

--- src/main/java/com/richard/demo/controller/HelloWorldController.java
+++ src/main/java/com/richard/demo/controller/HelloWorldController.java
@@ -24,14 +24,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.richard.demo.enums.OrderType;
 import com.richard.demo.model.Tickets;
